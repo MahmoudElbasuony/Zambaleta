b9925aa4a3736427278c886e4526369a2c023680
Mahmoud Elbasuony <melbasuony@STA.com>
2019-02-10 17:42:28 +0200

    Add project files.

---
+++ Zambaleta/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+
+namespace Zambaleta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
+
---
+++ Zambaleta/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zambaleta
+{
+    public partial class Form1 : Form
+    {
+        List<Thread> workers = new List<Thread>();
+        Graphics g;
+        Random r = new Random();
+        object lock_object = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+            BackColor = Color.LimeGreen;
+            TransparencyKey = BackColor;
+        }
+
+
+        void DrawCircles()
+        {
+            while (Thread.CurrentThread.IsAlive)
+            {
+                int pen_width = r.Next(Width / 15);
+                int red = ((int)(r.NextDouble() * 255));
+                int green = ((int)(r.NextDouble() * 255));
+                int blue = ((int)(r.NextDouble() * 255));
+                int x = ((int)(r.NextDouble() * Screen.PrimaryScreen.Bounds.Width));
+                int y = ((int)(r.NextDouble() * Screen.PrimaryScreen.Bounds.Height));
+                int circle_radius = r.Next(Width / 15);
+                Color color = Color.FromArgb(red, green, blue);
+                lock (lock_object)
+                    g.DrawEllipse(new Pen(color, pen_width), new Rectangle(x, y, circle_radius, circle_radius));
+                Thread.Sleep(100);
+            }
+        }
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread worker = new Thread(DrawCircles);
+                worker.IsBackground = false;
+                worker.Start();
+                workers.Add(worker);
+            }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            workers.ForEach(w => { w.Abort(); });
+            workers.Clear();
+            g.Dispose();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
